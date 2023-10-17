2a0869394a03a8fa3d3f6d4b124e705d0cd4872f
Beladric <kilian.goetz@yahoo.fr>
2023-10-17 13:54:23 +0200

    Create skeleton of the project
    Create an empty home page an a route just to define the structure of the frontend project

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import NavBar from "./components/navbar/navBar.tsx";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import HomePage from "./pages/homePage/homePage.tsx";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-  <NavBar/>
+    <NavBar/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )

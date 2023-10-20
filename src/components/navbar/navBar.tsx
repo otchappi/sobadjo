4ac1aaabfc18038f0bb43b25b461e5bedd826da0
Beladric <kilian.goetz@yahoo.fr>
2023-10-20 23:25:30 +0200

    Create login page

--- src/components/navbar/navBar.tsx
+++ src/components/navbar/navBar.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {
     AppBar,
-    Button,
     IconButton,
     Toolbar,
     Drawer,
--- src/pages/loginPage/loginPage.tsx
+++ src/pages/loginPage/loginPage.tsx
@@ -1,7 +1,56 @@
+import {Button, Grid, Paper, TextField, Typography, IconButton, InputAdornment, FormControl, InputLabel, OutlinedInput} from "@mui/material";
+import { useState } from "react";
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+
 export default function LoginPage(){
+    const [formValues, setFormValues] = useState({
+        email: '',
+        password: ''
+    });
+    const [showPassword, setShowPassword] = useState(false);
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+
     return(
-        <div style={{height:'100vh',width:'100%',display:'flex',alignItems:'center',justifyContent:'center'}}>
-            <h1>Login</h1>
-        </div>
+        <Grid container justifyContent="center" alignItems="center" style={{ height: '100vh' }}>
+            <Grid item xs={10} sm={6} md={4} lg={2}>
+                <Paper elevation={3} sx={{ padding:5, borderRadius:'15px' }}>
+                    <Typography variant="h5" sx={{textAlign:'center', mb: 2}}>
+                        Login
+                    </Typography>
+                    <Grid container direction="column" alignItems="center" sx={{gap: 2}}>
+                        <TextField
+                            label="Email"
+                            variant="outlined"
+                            value={formValues.email}
+                            onChange={(e) => setFormValues({...formValues, email: e.target.value})}
+                        />
+                        <FormControl variant="outlined">
+                            <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+                            <OutlinedInput
+                                id="outlined-adornment-password"
+                                type={showPassword ? 'text' : 'password'}
+                                onChange={(e)=>setFormValues({...formValues, password: e.target.value})}
+                                endAdornment={
+                                    <InputAdornment position="end">
+                                        <IconButton
+                                            aria-label="toggle password visibility"
+                                            onClick={handleClickShowPassword}
+                                            edge="end"
+                                        >
+                                            {showPassword ? <VisibilityOff /> : <Visibility />}
+                                        </IconButton>
+                                    </InputAdornment>
+                                }
+                                label="Password"
+                            />
+                        </FormControl>
+                        <Button variant="contained" color="primary">
+                            Login
+                        </Button>
+                    </Grid>
+                </Paper>
+            </Grid>
+        </Grid>
     )
 }
